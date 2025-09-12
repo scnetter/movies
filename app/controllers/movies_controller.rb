b30ac7181426041797d4fef8f33ed888ebfef8c4
Kelly Netterville <kellynetterville@Ozark-MacBook-Pro.local>
2025-09-12 08:49:15 -0500

    Tie Movie model to controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
 
     def index
-        @movies = ["Iron Man", "Superman", "Spider-man", "Pulp Fiction", "Forrest Gump"]
+        @movies = Movie.all
     end
 end
